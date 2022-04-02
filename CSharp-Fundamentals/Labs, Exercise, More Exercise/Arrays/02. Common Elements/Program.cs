504fcdb22c13fb3d99a9b447192a7b3ce72e690a
VladimirKotsev <v.kotsev04@gmail.com>
2022-04-02 09:35:00 +0300

    Fixed an issue

---
+++ CSharp-Fundamentals/Labs, Exercise, More Exercise/Arrays/02. Common Elements/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _02._Common_Elements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] firstArray = Console.ReadLine().Split(" ");
+            string[] secondArray = Console.ReadLine().Split(" ");
+            for (int i = 0; i < secondArray.Length; i++) // 10, hey, 4, hello
+            {
+                for (int j = 0; j < firstArray.Length; j++)  //Hey, hello, 2, 4
+                {
+                    if(firstArray[j] == secondArray[i])
+                    {
+                        Console.Write(firstArray[j] + " ");
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ CSharp-Fundamentals/Labs, Exercise, More Exercise/Arrays/06. Equal Sum/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+
+namespace _06._Equal_Sum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+            int sumLeft = 0;
+            int sumRight = 0;
+            sbyte index = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                for (int k = i - 1; k >= 0; k--)
+                {
+                    sumLeft += array[k];
+                }
+                for (int j = i+1; j < array.Length; j++)
+                {
+                    sumRight += array[j];
+                }
+                if (sumLeft == sumRight)
+                {
+                    index = (sbyte)i;
+                    Console.WriteLine(index);
+                    return;
+                }
+                sumRight = 0;
+                sumLeft = 0;
+            }
+            Console.WriteLine("no");
+        }
+    }
+}
