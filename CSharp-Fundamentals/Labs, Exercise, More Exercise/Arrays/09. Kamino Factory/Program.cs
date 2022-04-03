1f0313d8a859c9983a2c66d773d8f4568d3ba84f
VladimirKotsev <v.kotsev04@gmail.com>
2022-04-03 10:23:06 +0300

    Solved a problem

--- CSharp-Fundamentals/Labs, Exercise, More Exercise/Arrays/09. Kamino Factory/Program.cs
+++ CSharp-Fundamentals/Labs, Exercise, More Exercise/Arrays/09. Kamino Factory/Program.cs
@@ -7,28 +7,39 @@ namespace _09._Kamino_Factory
     {
         static void Main(string[] args)
         {
-            int n = int.Parse(Console.ReadLine());
+            int length = int.Parse(Console.ReadLine());
             string input = Console.ReadLine();
+            int index = length - 1;
+            int sample = 0;
+            int countFor = 1;
             int sum = 0;
-            int count = 0;
-            int index = 0;
-            int maxCount = -10000;
+            int outputSum = 0;
+            int[] outputArray = new int[length];
             while (input != "Clone them!")
             {
-                int[] array = input.Split("!").Select(int.Parse).ToArray();
-                for (int i = 0; i < array.Length; i++)
+                sum = 0;
+                int[] DNA = input.Split('!').Select(int.Parse).ToArray();
+                sum += DNA[length - 1];
+                for (int i = 0; i < DNA.Length - 1; i++)
                 {
-                    sum += array[i];
-                    if (array[i] == array[i + 1])
+                    if (DNA[i] == DNA[i + 1])
                     {
-                        count++;
-                        if (count > maxCount)
+                        if (i < index)
                         {
+                            sum += DNA[i];
+                            sample = countFor;
                             index = i;
+                            outputArray = DNA;
+                            outputSum = sum;
                         }
                     }
                 }
+                countFor++;
+
+                input = Console.ReadLine();
             }
+            Console.WriteLine($"Best DNA sample {sample} with sum: {outputSum + 1}.");
+            Console.WriteLine(String.Join(' ', outputArray));
         }
     }
 }
