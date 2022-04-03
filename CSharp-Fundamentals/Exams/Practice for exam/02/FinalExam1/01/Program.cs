8ae15b9c189e4731a4cbdc95a3ddb5a4a8b0b5ca
VladimirKotsev <v.kotsev04@gmail.com>
2022-04-03 09:24:55 +0300

    Renamed a folder

---
+++ CSharp-Fundamentals/Exams/Practice for exam/02/FinalExam1/01/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Text;
+
+namespace _01_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            StringBuilder stops = new StringBuilder();
+            string plan = Console.ReadLine();
+            stops.Append(plan);
+            string command = Console.ReadLine();
+            while (command != "Travel")
+            {
+                string[] cmd = command.Split(':');
+                switch (cmd[0])
+                {
+                    case "Add Stop":
+                        if (int.Parse(cmd[1]) >= 0 && int.Parse(cmd[1]) <= stops.Length - 1)
+                        {
+                            stops.Insert(int.Parse(cmd[1]), cmd[2]);
+                        }
+                        Console.WriteLine(stops.ToString());
+                        break;
+
+                    case "Remove Stop":
+                        int length = int.Parse(cmd[2]) - int.Parse(cmd[1]);
+                        if (int.Parse(cmd[1]) >= 0 && int.Parse(cmd[1]) <= stops.Length - 1 && int.Parse(cmd[2]) >= 0 && int.Parse(cmd[2]) <= stops.Length - 1)
+                        {
+                            stops.Remove(int.Parse(cmd[1]), length + 1);
+                        }
+                        Console.WriteLine(stops.ToString());
+                        break;
+
+                    case "Switch":
+                        stops.Replace(cmd[1], cmd[2]);
+                        Console.WriteLine(stops.ToString());
+                        break;
+                }
+                command = Console.ReadLine();
+            }
+            Console.WriteLine($"Ready for world tour! Planned stops: {stops.ToString()}");
+        }
+    }
+}
