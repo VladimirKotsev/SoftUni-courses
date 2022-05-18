76f904ad47ce2efc5e9e5ef3429b93fba76f9eb0
VladimirKotsev <v.kotsev04@gmail.com>
2022-05-18 07:51:45 +0300

    Sorting for Problems

---
+++ CSharp-Advanced/Stacks and Queues/Stacks and Queues - Lab/08. Traffic Jam/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _08._Traffic_Jam
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
