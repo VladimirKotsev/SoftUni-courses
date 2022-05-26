999e4d5943695fd367db413363855f41fe1a4ba1
VladimirKotsev <v.kotsev04@gmail.com>
2022-05-26 15:09:02 +0300

    Fixed 08.

--- CSharp-Advanced/Stacks and Queues/Stacks and Queues - Exercise/08. Balanced Parenthesis/Program.cs
+++ CSharp-Advanced/Stacks and Queues/Stacks and Queues - Exercise/08. Balanced Parenthesis/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Text;
 
 namespace _08._Balanced_Parenthesis
 {
@@ -7,50 +8,36 @@ namespace _08._Balanced_Parenthesis
     {
         static void Main(string[] args)
         {
-            Stack<int> openParentheses = new Stack<int>();
-            Stack<int> closedParentheses = new Stack<int>();
+            Stack<char> openParentheses = new Stack<char>();
             string input = Console.ReadLine();
-            for (int i = 0; i < input.Length / 2; i++)
-                openParentheses.Push(input[i]);
-            for (int i = input.Length - 1; i >= input.Length / 2; i--)
-                closedParentheses.Push(input[i]);
-            { }
-            while(openParentheses.Count != 0)
+            for (int i = 0; i < input.Length; i++)
             {
-                if (openParentheses.Peek() == 40 && closedParentheses.Peek() == 41)
+                if (input[i] == '(' || input[i] == '{' || input[i] == '[')
+                    openParentheses.Push(input[i]);
+                else if (input[i] == ')' || input[i] == '}' || input[i] == ']')
                 {
-                    openParentheses.Pop();
-                    closedParentheses.Pop();
-                }
-                else if (openParentheses.Peek() == 91 && closedParentheses.Peek() == 93)
-                {
-                    openParentheses.Pop();
-                    closedParentheses.Pop();
-                }
-                else if (openParentheses.Peek() == 123 && closedParentheses.Peek() == 125)
-                {
-                    openParentheses.Pop();
-                    closedParentheses.Pop();
-                }
-                else if (openParentheses.Peek() == 41 && closedParentheses.Peek() == 40)
-                {
-                    openParentheses.Pop();
-                    closedParentheses.Pop();
-                }
-                else if (openParentheses.Peek() == 93 && closedParentheses.Peek() == 91)
-                {
-                    openParentheses.Pop();
-                    closedParentheses.Pop();
-                }
-                else if (openParentheses.Peek() == 125 && closedParentheses.Peek() == 123)
-                {
-                    openParentheses.Pop();
-                    closedParentheses.Pop();
-                }
-                else
-                {
-                    Console.WriteLine("NO");
-                    return;
+                    if (openParentheses.Count == 0)
+                    {
+                        Console.WriteLine("NO");
+                        return;
+                    }
+                    else if (openParentheses.Peek() == '{' && input[i] == '}')
+                    {
+                        openParentheses.Pop();
+                    }
+                    else if (openParentheses.Peek() == '[' && input[i] == ']')
+                    {
+                        openParentheses.Pop();
+                    }
+                    else if (openParentheses.Peek() == '(' && input[i] == ')')
+                    {
+                        openParentheses.Pop();
+                    }
+                    else
+                    {
+                        Console.WriteLine("NO");
+                        return;
+                    }
                 }
             }
             Console.WriteLine("YES");
