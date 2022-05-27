d1e6527b1bd72b3fb70820d9574612b03ffc87ad
VladimirKotsev <v.kotsev04@gmail.com>
2022-05-27 22:56:50 +0300

    These are the 08. and 09.

--- CSharp-Advanced/Multidimensional Arrays/Multidimensional Arrays - Exercise/08. Bombs/Program.cs
+++ CSharp-Advanced/Multidimensional Arrays/Multidimensional Arrays - Exercise/08. Bombs/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace _08._Bombs
 {
@@ -6,7 +7,88 @@ namespace _08._Bombs
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int n = int.Parse(Console.ReadLine());
+            int[,] matrix = new int[n, n];
+            for (int i = 0; i < n; i++)
+            {
+                int[] line = Console.ReadLine()
+                    .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse)
+                    .ToArray();
+                for (int j = 0; j < n; j++)
+                {
+                    matrix[i, j] = line[j];
+                }
+            }
+            string[] input = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            foreach (string d in input)
+            {
+                int[] dimensions = d.Split(',', StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse)
+                    .ToArray();
+                int row = dimensions[0];
+                int col = dimensions[1];
+                if (matrix[row, col] > 0)
+                    matrix = GetNewMatrix(matrix, row, col);
+            }
+            int sum = 0;
+            int counter = 0;
+            for (int row = 0; row < n; row++)
+                for (int col = 0; col < n; col++)
+                {
+                    if (matrix[row, col] > 0)
+                    {
+                        counter++;
+                        sum += matrix[row, col];
+                    }
+                }
+            Console.WriteLine($"Alive cells: {counter}");
+            Console.WriteLine($"Sum: {sum}");
+            for (int row = 0; row < n; row++)
+            {
+                for (int col = 0; col < n; col++)
+                    Console.Write(matrix[row, col] + " ");
+                Console.WriteLine();
+            }
         }
+        static int[,] GetNewMatrix(int[,] copyMatrix, int row, int col)
+        {
+            int maxRow = copyMatrix.GetLength(0);
+            int maxCol = maxRow;
+            int power = copyMatrix[row, col];
+            if (row - 1 >= 0) // first row
+            {
+                if (copyMatrix[row - 1, col] > 0) // above bomb
+                    copyMatrix[row - 1, col] -= power;
+
+                if (col - 1 >= 0) // first row
+                    if (copyMatrix[row - 1, col - 1] > 0) // above and to the left of bomb
+                        copyMatrix[row - 1, col - 1] -= power;
+                if (col + 1 < maxCol) // first row
+                    if (copyMatrix[row - 1, col + 1] > 0) // above and to the right of bomb
+                        copyMatrix[row - 1, col + 1] -= power;
+            }
+            if (col - 1 >= 0) // second row
+                if (copyMatrix[row, col - 1] > 0) // to the left of bomb
+                    copyMatrix[row, col - 1] -= power;
+            if (col + 1 < maxCol) // second row
+                if (copyMatrix[row, col + 1] > 0) // to the right of bomb
+                    copyMatrix[row, col + 1] -= power;
+            if (row + 1 < maxRow) // third row
+            {
+                if (copyMatrix[row + 1, col] > 0) // below bomb
+                    copyMatrix[row + 1, col] -= power;
+
+                if (col + 1 < maxCol) // third row
+                    if (copyMatrix[row + 1, col + 1] > 0) //below and to the right of bomb
+                        copyMatrix[row + 1, col + 1] -= power;
+                if (col - 1 >= 0) // third row
+                    if (copyMatrix[row + 1, col - 1] > 0) // below and to the left of bomb
+                        copyMatrix[row + 1, col - 1] -= power;
+            }
+            copyMatrix[row, col] = 0; //bomb -> dead
+            return copyMatrix;
+        }
+
     }
 }
--- CSharp-Advanced/Multidimensional Arrays/Multidimensional Arrays - Exercise/09. Miner/Program.cs
+++ CSharp-Advanced/Multidimensional Arrays/Multidimensional Arrays - Exercise/09. Miner/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace _09._Miner
 {
@@ -6,7 +7,104 @@ namespace _09._Miner
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int n = int.Parse(Console.ReadLine());
+            char[,] field = new char[n, n];
+            string[] directions = Console.ReadLine().Split(' ');
+            int coalCounter = 0;
+            int currRow = 0;
+            int currCol = 0;
+            for (int row = 0; row < n; row++)
+            {
+                var line = Console.ReadLine().Split(' ');
+                for (int col = 0; col < n; col++)
+                {
+                    field[row, col] = char.Parse(line[col]);
+                    if (char.Parse(line[col]) == 'c')
+                        coalCounter++;
+                    else if (char.Parse(line[col]) == 's')
+                    {
+                        currRow = row;
+                        currCol = col;
+                    }
+                }
+            }
+            foreach (string direction in directions)
+            {
+                switch (direction)
+                {
+                    case "up":
+                        if (currRow - 1 >= 0)
+                        {
+                            currRow--;
+                            if (field[currRow, currCol] == 'c')
+                            {
+                                coalCounter--;
+                                field[currRow, currCol] = '*';
+                            }
+                            else if (field[currRow, currCol] == 'e')
+                            {
+                                Console.WriteLine($"Game over! ({currRow}, {currCol})");
+                                return;
+                            }
+                            
+                        }
+                        break;
+                    case "right":
+                        if (currCol + 1 < n)
+                        {
+                            currCol++;
+                            if (field[currRow, currCol] == 'c')
+                            {
+                                coalCounter--;
+                                field[currRow, currCol] = '*';
+                            }
+                            else if (field[currRow, currCol] == 'e')
+                            {
+                                Console.WriteLine($"Game over! ({currRow}, {currCol})");
+                                return;
+                            }
+                        }
+                        break;
+                    case "left":
+                        if (currCol - 1 >= 0)
+                        {
+                            currCol--;
+                            if (field[currRow, currCol] == 'c')
+                            {
+                                coalCounter--;
+                                field[currRow, currCol] = '*';
+                            }
+                            else if (field[currRow, currCol] == 'e')
+                            {
+                                Console.WriteLine($"Game over! ({currRow}, {currCol})");
+                                return;
+                            }
+                        }
+                        break;
+                    case "down":
+                        if (currRow + 1 < n)
+                        {
+                            currRow++;
+                            if (field[currRow, currCol] == 'c')
+                            {
+                                coalCounter--;
+                                field[currRow, currCol] = '*';
+                            }
+                            else if (field[currRow, currCol] == 'e')
+                            {
+                                Console.WriteLine($"Game over! ({currRow}, {currCol})");
+                                return;
+                            }
+                        }
+                        break;
+                }
+                if (coalCounter == 0)
+                {
+                    Console.WriteLine($"You collected all coals! ({currRow}, {currCol})");
+                    return;
+                }
+            }
+            Console.WriteLine($"{coalCounter} coals left. ({currRow}, {currCol})");
         }
     }
 }
