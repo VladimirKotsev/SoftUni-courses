f8e876ce005a899ebec664bb8f1b57aaf4b06c3d
VladimirKotsev <v.kotsev04@gmail.com>
2022-06-06 21:38:12 +0300

    Solved a problem

--- CSharp-Advanced/Defining Classes/Defining Classes - Exercise/01. Define a Class Person/Car.cs
+++ CSharp-Advanced/Defining Classes/Defining Classes - Exercise/01. Define a Class Person/Car.cs
@@ -6,6 +6,9 @@ namespace DefiningClasses
 {
     public class Car
     {
+        public Engine engine;
+        public List<Tires> tires = new List<Tires>();
+        public Cargo cargo;
         private string model;
         public string Model
         {
@@ -43,6 +46,17 @@ namespace DefiningClasses
             this.FuelAmount = fuelAmount;
             this.FuelConsumptionPerKilometer = fuelConsumptionPerKilometer;
             this.TravelledDistance = 0; 
+        }
+        public Car
+            (string model, int engineSpeed, int enginePower, int cargoWeight, string cargoType, List<string> infoTire1, List<string> infoTire2, List<string> infoTire3, List<string> infoTire4)
+        {
+            this.Model = model;
+            this.engine = new Engine(engineSpeed, enginePower);
+            this.cargo = new Cargo(cargoWeight, cargoType);
+            tires.Add(new Tires(double.Parse(infoTire1[0]), int.Parse(infoTire1[1])));
+            tires.Add(new Tires(double.Parse(infoTire2[0]), int.Parse(infoTire2[1])));
+            tires.Add(new Tires(double.Parse(infoTire3[0]), int.Parse(infoTire3[1])));
+            tires.Add(new Tires(double.Parse(infoTire4[0]), int.Parse(infoTire4[1])));
         }
         public List<Car> Drive(List<Car> cars, string car, int kilometers)
         {
--- CSharp-Advanced/Defining Classes/Defining Classes - Exercise/01. Define a Class Person/StartUp.cs
+++ CSharp-Advanced/Defining Classes/Defining Classes - Exercise/01. Define a Class Person/StartUp.cs
@@ -19,7 +19,7 @@ namespace DefiningClasses
             //}
             //Console.WriteLine(family.GetOldestMember().Name + ' ' + family.GetOldestMember().Age);
 
-            //04. Opinion Poll]
+            //04. Opinion Poll
             //int n = int.Parse(Console.ReadLine());
             //List<Person> people = new List<Person>();
             //for (int i = 1; i <= n; i++)
@@ -35,30 +35,83 @@ namespace DefiningClasses
             //}
 
             //06. Speed Racing
+            //int n = int.Parse(Console.ReadLine());
+            //List<Car> cars = new List<Car>();
+            //Car car = new Car();
+            //for (int i = 1; i <= n; i++)
+            //{
+            //    string[] input = Console.ReadLine().Split(' ');
+            //    Car newCar = new Car(input[0], double.Parse(input[1]), double.Parse(input[2]));
+            //    cars.Add(newCar);
+            //}
+            //var info = Console.ReadLine().Split(' ');
+            //while (info[0] != "End")
+            //{
+            //    car.Drive(cars, info[1], int.Parse(info[2]));
+            //    info = Console.ReadLine().Split(' ');
+            //}
+            //PrintOut(cars);
+
+            //07. Raw Data
             int n = int.Parse(Console.ReadLine());
             List<Car> cars = new List<Car>();
-            Car car = new Car();
             for (int i = 1; i <= n; i++)
             {
                 string[] input = Console.ReadLine().Split(' ');
-                Car newCar = new Car(input[0], double.Parse(input[1]), double.Parse(input[2]));
+                string model = input[0];
+                int speed = int.Parse(input[1]);
+                int power = int.Parse(input[2]);
+                int weight = int.Parse(input[3]);
+                string type = input[4];
+                List<string> infoTire1 = new List<string>()
+                {
+                    input[5], //pressure
+                    input[6] //age
+                };
+                List<string> infoTire2 = new List<string>()
+                {
+                    input[7], //pressure
+                    input[8] //age
+                };
+                List<string> infoTire3 = new List<string>()
+                {
+                    input[9], //pressure
+                    input[10] //age
+                };
+                List<string> infoTire4 = new List<string>()
+                {
+                    input[11], //pressure
+                    input[12]  //age
+                };
+                Car newCar = new Car(model, speed, power, weight, type, infoTire1, infoTire2, infoTire3, infoTire4);
                 cars.Add(newCar);
             }
-            var info = Console.ReadLine().Split(' ');
-            while (info[0] != "End")
+
+            string cmd = Console.ReadLine();
+            switch(cmd)
             {
-                car.Drive(cars, info[1], int.Parse(info[2]));
-                info = Console.ReadLine().Split(' ');
+                case "fragile":
+
+                    break;
+                case "flammable":
+
+                    break;
             }
-            PrintOut(cars);
         }
         //06. Speed Racing
-        static void PrintOut(List<Car> cars)
+        //static void PrintOut(List<Car> cars)
+        //{
+        //    foreach(Car car in cars)
+        //    {
+        //        Console.WriteLine($"{car.Model} {car.FuelAmount:f2} {car.TravelledDistance}");
+        //    }
+        //}
+        //07. Raw Data
+        static void PrintOutFragile(List<Car> cars)
         {
-            foreach(Car car in cars)
-            {
-                Console.WriteLine($"{car.Model} {car.FuelAmount:f2} {car.TravelledDistance}");
-            }
+            List<Car> filtered = cars.Where(x => x.cargo.Type == "fragile").ToList();
+            if (filtered.Tire)
+            
         }
     }
 }
