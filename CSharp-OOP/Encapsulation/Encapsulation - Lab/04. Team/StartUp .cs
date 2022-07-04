1d68c298d93fd71d92179e3fd1ad0124198800ec
VladimirKotsev <v.kotsev04@gmail.com>
2022-07-04 23:34:40 +0300

    Solved Lab with 1 incorrect test

---
+++ CSharp-OOP/Encapsulation/Encapsulation - Lab/04. Team/StartUp .cs
@@ -0,0 +1,30 @@
+﻿namespace PersonsInfo
+{
+    using System;
+    using System.Collections.Generic;
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            List<Person> persons = new List<Person>();
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                string[] line = Console.ReadLine().Split(' ');
+                Person newPerson = new Person(line[0], line[1], int.Parse(line[2]), decimal.Parse(line[3]));
+                persons.Add(newPerson);
+            }
+
+            Team team = new Team("SoftUni");
+
+            foreach (Person person in persons)
+            {
+                team.AddPlayer(person);
+            }
+
+            Console.WriteLine($"First team has {team.FirstTeam.Count} players.");
+            Console.WriteLine($"Reserve team has {team.ReserveTeam.Count} players.");
+
+        }
+    }
+}
---
+++ CSharp-OOP/Encapsulation/Encapsulation - Lab/04. Team/Team.cs
@@ -0,0 +1,35 @@
+﻿namespace PersonsInfo
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+    public class Team
+    {
+        private string name;
+        private List<Person> firstTeam;
+        public IReadOnlyCollection<Person> FirstTeam
+        {
+            get { return this.firstTeam.AsReadOnly(); }
+        }
+        private List<Person> reserveTeam;
+        public IReadOnlyCollection<Person> ReserveTeam
+        {
+            get { return this.reserveTeam.AsReadOnly(); }
+        }
+
+        public Team(string name)
+        {
+            this.name = name;
+            this.firstTeam = new List<Person>();
+            this.reserveTeam = new List<Person>();
+        }
+
+        public void AddPlayer(Person person)
+        {
+            if (person.Age < 40)
+                { firstTeam.Add(person); }
+            else
+                { reserveTeam.Add(person); }
+        }
+    }
+}
