29e0045fb8180fd3a1c5c7be055a27ff590be7e2
VladimirKotsev <v.kotsev04@gmail.com>
2022-09-14 16:59:25 +0300

    More from Exercise

--- JS-Advanced/Syntax, Function and Statements/Syntax, Function and Statements - Exercise/05. Time to Walk.js
+++ JS-Advanced/Syntax, Function and Statements/Syntax, Function and Statements - Exercise/05. Time to Walk.js
@@ -1,7 +1,7 @@
 function walk(steps, footprint, km){
 
     let distance = steps * footprint;
-    let countOfBreaks = distance / 500;
+    let countOfBreaks = Math.floor(distance / 500);
     let mPerKilometer = km / 3.6;
 
     let totalTime = countOfBreaks * 60;
@@ -10,10 +10,13 @@ function walk(steps, footprint, km){
 
     let hours = totalTime / 3600;
     let minutes = totalTime / 60;
+    
+    hours = Math.floor(hours);
+    minutes = Math.floor(minutes);
+    let seconds = Number(totalTime - (minutes*60)).toFixed(0);
 
-
-    console.log(totalTime);
+    let formatedHours = hours<10 ? `0${hours}` : `${hours}`;
+    let formatedMinutes = minutes<10 ? `0${minutes}` : `${minutes}`;
+    let formatedSeconds = seconds<10 ? `0${seconds}` : `${seconds}`;
+    console.log(`${formatedHours}:${formatedMinutes}:${formatedSeconds}`);
 }
-
-walk(4000, 0.60, 5);
-walk(2564, 0.70, 5.5);
---
+++ JS-Advanced/Syntax, Function and Statements/Syntax, Function and Statements - Exercise/06. Road Radar.js
@@ -0,0 +1,39 @@
+function radar(speed, area){
+
+    let speedLimit = Number(0);
+    switch(area)
+    {
+        case `city`: speedLimit = Number(50); break;
+        case `interstate`: speedLimit = Number(90); break;
+        case `motorway`: speedLimit = Number(130); break;
+        case `residential`: speedLimit = Number(20); break;
+    }
+
+    if (speed <= speedLimit)
+    {
+        console.log(`Driving ${speed} km/h in a ${speedLimit} zone`)
+    }
+    else
+    {
+        let status = ``;
+        let diff = speed - speedLimit;
+        if (diff <= 20)
+        {
+            status = `speeding`;
+        }
+        else if (diff <= 40)
+        {
+            status = `excessive speeding`;
+        }
+        else
+        {
+            status = `reckless driving`;
+        }
+
+        console.log(`The speed is ${diff} km/h faster than the allowed speed of ${speedLimit} - ${status}`);
+    }
+
+}
+
+radar(40, `city`);
+radar(21, `residential`);
