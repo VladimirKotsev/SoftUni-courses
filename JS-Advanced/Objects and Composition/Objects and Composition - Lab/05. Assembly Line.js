26792f1e17bd80eb1b1138ba7b61839427155490
VladimirKotsev <v.kotsev04@gmail.com>
2022-09-22 11:48:46 +0300

    Solved left problems!

--- JS-Advanced/Objects and Composition/Objects and Composition - Lab/05. Assembly Line.js
+++ JS-Advanced/Objects and Composition/Objects and Composition - Lab/05. Assembly Line.js
@@ -1,22 +1,46 @@
-const assemblyLine = createAssemblyLine();
-
-const myCar = {
-    make: 'Toyota',
-    model: 'Avensis'
-};
-
-assemblyLine.hasClima(myCar);
-console.log(myCar.temp);
-myCar.tempSettings = 18;
-myCar.adjustTemp();
-console.log(myCar.temp);
-
 function createAssemblyLine(){
+
     myObj = {
-        hasClima: function (myCar){
-            temp: 
+        hasClima(myCar){
+            myCar.temp = Number(21);
+            myCar.tempSettings = Number(21);
+            myCar.adjustTemp = function() {
+
+                if (this.temp < this.tempSettings){
+                    this.temp++;
+                }
+                else if (this.temp > this.tempSettings){
+                    this.temp--;
+                }
+            }            
+        },            
+        hasAudio(myCar){
+            myCar.currentTrack = {};
+            myCar.nowPlaying = function() {
+                console.log(`Now playing '${this.currentTrack.name}' by ${this.currentTrack.artist}`);
+            }
+        },
+        hasParktronic(myCar){
+
+            myCar.checkDistance = function(number) {
+                number = Number(number);
+                if (number < 0.1){
+                    console.log("Beep! Beep! Beep!");
+                }
+                else if (number < 0.25){
+                    console.log("Beep! Beep!");
+                }
+                else if (number < 0.5){
+                    console.log("Beep!");   
+                }
+                else{
+                    console.log(``);
+                }
+            }
         }
 
     };
 
+    return myObj;
 }
+
--- JS-Advanced/Objects and Composition/Objects and Composition - Lab/06. From JSON to HTML Table.js
+++ JS-Advanced/Objects and Composition/Objects and Composition - Lab/06. From JSON to HTML Table.js
@@ -1,25 +1,65 @@
-function toHTML(arr){
-    console.log(`<table>`);
-    console.log(`<tr><th>Name</th><th>Score</th></tr>`);  
-    for (let element of arr){
-        console.log(`<tr><th>${element["Name"]}</th><th>${element["Score"]}</th></tr>`);
-    }
-    console.log(`</table>`)
-}
+function toHTML(input){
 
-toHTML(`[{"Name":"Stamat",
-"Score":5.5},
-{"Name":"Rumen",
-"Score":6}]`
-);
-console.log(`-------------`);
-toHTML(`[{"Name":"Pesho",
-"Score":4,
-" Grade":8},
-{"Name":"Gosho",
-"Score":5,
-" Grade":8},
-{"Name":"Angel",
-"Score":5.50,
-" Grade":10}]`
-);
+    let arr = JSON.parse(input);
+    let HTML = ``;
+
+    //html table open
+    HTML += "<table>\n"
+
+    //header
+    HTML += HTMLHeader();
+
+    //body
+    HTML += HTMLBody();
+
+    //html table close
+    HTML += `</table>`
+    
+    //print
+    console.log(HTML);
+    
+
+
+    function HTMLEscape(value){
+        
+        return value
+                .toString()
+                .replace(/&/g, '&amp;')
+                .replace(/</g, '&lt;')
+                .replace(/>/g, '&gt;')
+                .replace(/"/g, '&quot;')
+                .replace(/'/g, '&#39;');
+
+    }
+    function HTMLHeader(){
+
+        let concat = `   <tr>`
+
+        for(let key of Object.keys(arr[0])){
+            concat += `<th>${HTMLEscape(key)}</th>`;
+        }
+
+        concat += `</tr>`
+        concat += `\n`;
+
+        return concat;
+    }
+    function HTMLBody(){
+
+        let concat = ``;
+
+        for(let element of arr){
+
+            concat += `   <tr>`;
+
+            for(let value of Object.values(element)){
+                concat += `<td>${HTMLEscape(value)}</td>`;
+            }
+
+            concat += `</tr>`;
+            concat += `\n`;
+        }
+
+        return concat;
+    }
+}
