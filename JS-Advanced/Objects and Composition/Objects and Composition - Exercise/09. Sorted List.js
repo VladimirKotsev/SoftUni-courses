2f704f5cb578c27bccd6d2e90714d46dbcf2b4b1
VladimirKotsev <v.kotsev04@gmail.com>
2022-09-24 22:43:28 +0300

    Fixed an issue!

--- JS-Advanced/Objects and Composition/Objects and Composition - Exercise/09. Sorted List.js
+++ JS-Advanced/Objects and Composition/Objects and Composition - Exercise/09. Sorted List.js
@@ -30,12 +30,3 @@ function createSortedList(){
 
     return obj;
 }
-
-let list = createSortedList();
-list.add(5);
-list.add(6);
-list.add(7);
-console.log(list.get(1)); 
-list.remove(1);
-console.log(list.get(1));
-console.log(list.size);
