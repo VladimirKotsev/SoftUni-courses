3d97fe52d3d92a77d84fcc3e30c6be694b960b44
VladimirKotsev <v.kotsev04@gmail.com>
2022-10-10 19:58:49 +0300

    Some changes

--- JS-Advanced/Advanced Functions/Advanced Functions - Exercise/08. Next Article/script.js
+++ JS-Advanced/Advanced Functions/Advanced Functions - Exercise/08. Next Article/script.js
@@ -1,3 +1,15 @@
 function getArticleGenerator(articles) {
-    // TODO
+
+    let arr = articles;
+    let div = document.getElementById(`content`);
+
+    return function(){
+        
+        if (arr.length === 0){
+
+            return;
+        }
+        let arti = arr.shift();
+        div.innerHTML += `<article>${arti}</article>`;
+    } 
 }
--- JS-Advanced/Advanced Functions/Advanced Functions - Exercise/09. Task Manager/app.js
+++ JS-Advanced/Advanced Functions/Advanced Functions - Exercise/09. Task Manager/app.js
@@ -1,3 +1,16 @@
 function solve() {
-    console.log("//TODO")
+    
+    let taksField = document.getElementById(`task`);
+    let descriptionField = document.getElementById(`description`);
+    let dataField = document.getElementById(`date`);
+
+    if (taksField.value === `` || descriptionField.textContent === `` || dataField.value === ``){
+
+        return;
+    }
+
+    let addBtn = document.getElementById(`add`);
+    addBtn.addEventListener(`click`, function(){
+
+    });
 }
