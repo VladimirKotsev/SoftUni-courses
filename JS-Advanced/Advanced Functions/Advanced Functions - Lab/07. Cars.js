85b36508082787ee4be10b09bb30935eaeb3e92f
VladimirKotsev <v.kotsev04@gmail.com>
2022-10-17 15:32:55 +0300

    Solved one problem

--- JS-Advanced/Advanced Functions/Advanced Functions - Lab/07. Cars.js
+++ JS-Advanced/Advanced Functions/Advanced Functions - Lab/07. Cars.js
@@ -2,48 +2,89 @@ function cars(arr){
 
     let result = [];
 
-    let obj = {
+    for (let i = 0; i < arr.length; i++) {
+        
+        let [cmd, name] = arr[i].split(` `);
 
-        set: function(name, key ,value){
+        if (arr[i].split(` `).length === 2){
 
-            result.filter(x => x.name === name)[key] = value;
-        },
-        print: function(name) {
+            switch(cmd){
 
-            let currentObj = result.filter(x => x.name === name);
-            let result = ``;
-            for (let obj of currentObj) {
-                
-                
+                case `create`:
+                    let obj = {
+                        name: name
+                    };
+
+                    result.push(obj);
+                    break;
+                case `print`:
+
+                    let resString = [];
+                    for (let key in result.find(x => x.name == name)) {
+                        
+                        if (key !== `inherit` && key !== `name`){
+
+                            resString.push(`${key}:${result.find(x => x.name == name)[key]}`);
+                        }
+                    }
+
+                    if (result.find(x => x.name == name).inherit){
+
+                        inherit(name);
+                        //resString.reverse();
+                    }
+
+                    console.log(resString.join(`,`));
+
+                    function inherit(parentName){
+
+                        if (result.find(x => x.name == parentName).inherit){
+
+                            let nameOfInherit = result.find(x => x.name == parentName).inherit;
+                            
+                            for (let key in result.find(x => x.name == nameOfInherit)) {
+                                
+                                if (key !== `inherit` && key !== `name`){
+                                    
+                                    resString.push(`${key}:${result.find(x => x.name == nameOfInherit)[key]}`);
+                                }
+                            }
+                            if (result.find(x => x.name == nameOfInherit).inherit){
+
+                                inherit(nameOfInherit);
+                            }
+                        }
+                    }
+                    break;
             }
         }
-    };
+        else if (arr[i].split(` `).length === 4){
 
-    for (let index = 0; index < arr.length; index++) {
-        
-        let cmd = arr[index].split(` `)[0];
-        let name = arr[index].split(` `)[1];
+            let propertyOrIneherit = arr[i].split(` `)[2];
+            let nameOrValue = arr[i].split(` `)[3];
 
-        let key = ``;
-        let value = ``;
-        if (arr[index].split(` `).length === 4){
-            
-            keyOrInherits = arr[index].split(` `)[2];
-            valueOrParentName = arr[index].split(` `)[3];
+            switch (cmd){
+
+                case `create`:
+
+                    let obj = Object.assign({}, result.find(x => x.name == nameOrValue));
+                    obj.name = name;
+                    obj.inherit = nameOrValue;
+                    result.push(obj);
+                break;
+
+                case `set`:
+
+                    result.find(x => x.name == name)[propertyOrIneherit] = nameOrValue;
+                    
+                break;
+            }
+
         }
 
-        switch (cmd){
-            case `set`: set(name, keyOrInherits, valueOrParentName);
-            case ``
-        }
     }
 
 }
 
-cars(['create c1',
-'create c2 inherit c1',
-'set c1 color red',
-'set c2 model new',
-'print c1',
-'print c2']
-);
+//cars([`create pesho`, `create gosho inherit pesho`, `create stamat inherit gosho`, `set pesho rank number1`, `set gosho nich goshko`, `print stamat`]);
+//cars(['create c1','create c2 inherit c1','set c1 color red','set c2 model new','print c1','print c2']);
