55a4fb074e16286dd2a73f29d8c18cb2386b83e5
VladimirKotsev <v.kotsev04@gmail.com>
2022-10-17 13:32:29 +0300

    Solved one problem

--- JS-Advanced/DOM Introduction/DOM Introduction - Exercise/08. Generate Report/report.js
+++ JS-Advanced/DOM Introduction/DOM Introduction - Exercise/08. Generate Report/report.js
@@ -1,3 +1,34 @@
 function generateReport() {
-    //TODO
+    
+    let result = [];
+
+    let names = [];
+    let indexes = [];
+    let checkboxes = document.getElementsByTagName(`input`);
+    
+    for (let index = 0; index < checkboxes.length; index++) {
+        
+        if (checkboxes[index].checked){
+            
+            names.push(checkboxes[index].name);
+            indexes.push(index);
+        }
+    };
+    
+    let rows = document.getElementsByTagName(`tr`);
+
+    for (let i = 1; i < rows.length; i++) {
+        
+        let obj = {};
+
+        for (let index = 0; index < indexes.length; index++){
+
+            obj[names[index]] = rows[i].getElementsByTagName(`td`)[indexes[index]].textContent;
+        }
+
+        result.push(obj);
+    }
+
+    document.getElementById(`output`).textContent = JSON.stringify(result);
+
 }
